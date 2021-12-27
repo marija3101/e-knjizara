2c4dbe787a5f54ff62baef9bec024435b09a455c
Andjela Kostic <andjelakostic110@gmail.com>
2021-12-27 08:35:08 +0100

    Napravljeni seeder-i i pomoću DatabaseSeeder-a popunjena baza

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Author;
+use App\Models\Book;
+use App\Models\PublicationCity;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +17,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+
+        Book::truncate();
+        User::truncate();
+        Author::truncate();
+        PublicationCity::truncate();
+
+        $user = User::factory()->create();
+        $author1 = Author::factory()->create();
+        $author2 = Author::factory()->create();
+        $city1 = PublicationCity::factory()->create();
+        $city2 = PublicationCity::factory()->create();
+        $city3 = PublicationCity::factory()->create();
+
+        Book::factory(5)->create([
+            'user_id' => $user->id,
+            'author_id' => $author1->id,
+            'city_id' => $city1->id
+        ]);
+
+        Book::factory(3)->create([
+            'user_id' => $user->id,
+            'author_id' => $author1->id,
+            'city_id' => $city2->id
+        ]);
+        Book::factory(2)->create([
+            'user_id' => $user->id,
+            'author_id' => $author2->id,
+            'city_id' => $city3->id
+        ]);
     }
 }
