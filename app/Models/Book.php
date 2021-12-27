30342de5b931021cc897fa15edc43cca0dd3bf3c
Andjela Kostic <andjelakostic110@gmail.com>
2021-12-27 08:28:22 +0100

    Kreirani i međusobno povezani modeli Author, Book, PublicationCity i User

---
+++ app/Models/Book.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+    public function author()
+    {
+        return $this->belongsTo(Author::class);
+    }
+    public function publicationCity()
+    {
+        return $this->belongsTo(PublicationCity::class);
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
