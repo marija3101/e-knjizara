30a36b7fb5f6c42225f08d2214dfc75ec5953f3a
Andjela Kostic <andjelakostic110@gmail.com>
2021-12-27 08:33:12 +0100

    Kreirani factory fajlovi

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+use App\Models\Author;
+use App\Models\PublicationCity;
+
+class BookFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'slug' => $this->faker->slug(),
+            'title' => $this->faker->title(),
+            'description' => $this->faker->paragraph(),
+            'user_id' => User::factory(),
+            'author_id' => Author::factory(),
+            'city_id' => PublicationCity::factory()
+        ];
+    }
+}
---
+++ database/factories/PublicationCityFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PublicationCityFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'slug' => $this->faker->slug(),
+            'name' => $this->faker->word(),
+            'zip_code' => $this->faker->word()
+        ];
+    }
+}
