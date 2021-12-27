9a256ec7adaae1b0c1de2fae8dab3d7fb4e80001
Andjela Kostic <andjelakostic110@gmail.com>
2021-12-27 10:41:42 +0100

    Kreirane api rute

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\PublicationCity;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,25 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('user.book', UserBookController::class)->only(['index']);
+Route::resource('author.book', AuthorBooksController::class)->only(['index']);
+Route::resource('city.book', StarringSeriesController::class)->only(['index']);
+
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/profiles', function (Request $request) {
+        return auth()->user();
+    });
+    Route::resource('books', BookController::class)->only(['update', 'store', 'destroy']);
+    Route::resource('authors', AuthorController::class)->only(['update', 'store', 'destroy']);
+    Route::resource('city', PublicationCityController::class)->only(['update', 'store', 'destroy']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+Route::resource('books', BookController::class)->only(['index', 'show']);
+Route::resource('authors', AuthorController::class)->only(['index', 'show']);
+Route::resource('city', PublicationCityController::class)->only(['index', 'show']);
