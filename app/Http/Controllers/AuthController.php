506c61d837aead4305c56caa563a15aba94444c0
Marija Martinovic <marijaamartinovicc11@gmail.com>
2022-01-11 23:39:07 +0100

    Napravljene izmene u Kernel.php fajlu i AuthController-u

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -16,29 +16,57 @@ class AuthController extends Controller
         $validator = Validator::make(
             $request->all(),
             [
-                'password' => ['required', Password::min(8)->mixedCase()], 'name' => 'required|string|max:100', 'email' => 'required|string|max:100|email'
+                 'name' => 'required|string|max:100', 'email' => 'required|string|max:100|email','password' => ['required', Password::min(8)->mixedCase()] //|unique:users,email
             ]
         );
-        if ($validator->fails()) return response()->json($validator->errors());
+        if ($validator->fails()) return response()->json(['validation_errors'=>$validator->messages(),]);  //$validator->errors()
 
         $user = User::create(['name' => $request->name, 'email' => $request->email, 'password' => Hash::make($request->password)]);
 
-        $token = $user->createToken('auth_token')->plainTextToken;
-        return response()->json(['data' => $user, 'acess_token' => $token, 'token_type' => 'Bearer']);
+        $token = $user->createToken($user->email.'_Token')->plainTextToken; //'auth_token'
+        return response()->json(['status'=>200, 'name'=>$user->name, 'token'=>$token,'message'=>'Registred Success']); //'data' => $user, 'acess_token' => $token, 'token_type' => 'Bearer'
     }
 
     public function login(Request $request)
     {
-        if (!Auth::attempt($request->only('name', 'password'))) return response()->json(['message' => 'Unauthorized'], 401);
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'email' => 'required|max:100|email','password' => 'required' //|unique:users,email
+            ]
+        );
+        if ($validator->fails()) return response()->json(['validation_errors'=>$validator->messages(),]);
+        else {
+            $user = User::where('email', $request->email)->first();
+
+            if (! $user || ! Hash::check($request->password, $user->password)) {
+                return response()->json([
+              'status'=>401,
+              'message'=>'Invalid Credentials',
+                ]);
+            }
+            else {
+                if($user->role==1) {
+                    $token = $user->createToken($user->email.'_AdminToken',['server:admin'])->plainTextToken;  
+                }
+                else {
+                    $token = $user->createToken($user->email.'_Token',[''])->plainTextToken; 
+                }
+                
+                return response()->json(['status'=>200, 'name'=>$user->name, 'token'=>$token,'message'=>'Logged in Success']);
+            }
+        }
+
+        /*if (!Auth::attempt($request->only('email', 'password'))) return response()->json(['success'=>false]);
         $user = User::where('name', $request['name'])->firstOrFail();
 
         $token = $user->createToken('auth_token')->plainTextToken;
-        return response()->json(['message' => 'hi ' . $user->name . ' welcome', 'acess_token' => $token, 'token_type' => 'Bearer']);
+        return response()->json(['success'=>true, 'acess_token' => $token, 'token_type' => 'Bearer']);*/
     }
 
     public function logout()
     {
         auth()->user()->tokens()->delete();
-        return ['message' => 'You have logout'];
+        return ['status'=>200,'message' => 'You have logout'];
     }
 }
