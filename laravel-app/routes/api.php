8634bc020139526661d97bb2679eb7c3a96669d4
Jovana Miladinovic <mjovanaam@gmail.com>
2022-01-18 22:52:04 +0100

    Dodate nove rute

--- laravel-app/routes/api.php
+++ laravel-app/routes/api.php
@@ -11,6 +11,8 @@ use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\PublicationCityBookController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FrontendController;
+use App\Http\Controllers\CartController;
 
 /*
 |--------------------------------------------------------------------------
@@ -35,26 +37,59 @@ Route::post('/register', [AuthController::class, 'register']);
 
 Route::post('/login', [AuthController::class, 'login']);
 
+Route::get('getAuthor',[FrontendController::class,'author']);
+
+Route::get('fetchbooks/{slug}',[FrontendController::class,'book']);
+
+Route::get('viewbookdetail/{author_slug}/{book_slug}',[FrontendController::class,'viewbook']);
+
+Route::get('cart', [CartController::class, 'viewcart']);
+
+Route::put('cart-updatequantity/{cart_id}/{scope}',[CartController::class,'updatequantity']);
+
+Route::delete('delete-cartitem/{cart_id}',[CartController::class,'deleteCartitem']);
+
+Route::post('add-to-cart',[CartController::class,'addtocart']);
+
 Route::group(['middleware' => ['auth:sanctum','isAdmin']], function () {
-    /*Route::get('/profiles', function (Request $request) {
-        return auth()->user();
-    });*/
+
     Route::get('/checkingAuthenticated', function () {
         return response()->json(['message'=>'You are in', 'status'=>200],200);
     });
     Route::resource('books', BookController::class)->only(['update', 'store', 'destroy']);
     Route::resource('store-author', AuthorController::class)->only(['store']);
-    //Route::post('store-author', AuthorController::class, 'store');
-    Route::resource('cities', PublicationCityController::class)->only(['update', 'store', 'destroy']);
+    Route::resource('store-city', PublicationCityController::class)->only(['store']);
+
+    
    
+    Route::resource('cities', PublicationCityController::class)->only(['update', 'store', 'destroy']);
+
+    Route::get('all-authors', [AuthorController::class, 'allauthors']);
+    Route::get('all-citis', [PublicationCityController::class, 'allcitis']);
+    Route::get('all-users', [UserController::class, 'allusers']);
+    
+    Route::resource('store-book', BookController::class)->only(['store']);
+
+    Route::get('edit-author/{id}',[AuthorController::class,'edit']);
+Route::put('update-author/{id}',[AuthorController::class,'update']);
+Route::delete('delete-author/{id}',[AuthorController::class,'destroy']);
+Route::get('edit-book/{id}', [BookController::class,'edit']);
+Route::post('update-book/{id}', [BookController::class,'update']);
+Route::get('edit-city/{id}', [PublicationCityController::class,'edit']);
+Route::post('update-city/{id}', [PublicationCityController::class,'update']);
+Route::delete('delete-book/{id}',[BookController::class,'destroy']);
+Route::delete('delete-city/{id}',[PublicationCityController::class,'destroy']);
+
 });
 Route::group(['middleware' => ['auth:sanctum']], function () {
-    /*Route::get('/profiles', function (Request $request) {
-        return auth()->user();
-    });*/
+
     Route::post('/logout', [AuthController::class, 'logout']);
 });
 
+
+
 Route::resource('books', BookController::class)->only(['index', 'show']);
 Route::resource('view-author', AuthorController::class)->only(['index']);
+Route::resource('view-book', BookController::class)->only(['index']);
+Route::resource('view-city', PublicationCityController::class)->only(['index']);
 Route::resource('cities', PublicationCityController::class)->only(['index', 'show']);
