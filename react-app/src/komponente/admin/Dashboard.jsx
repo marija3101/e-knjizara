27b30db49d84eff809bfaac74396a694705897b3
Marija Martinovic <marijaamartinovicc11@gmail.com>
2024-01-25 20:52:29 +0100

    Preuredjen Dashboard

--- react-app/src/komponente/admin/Dashboard.jsx
+++ react-app/src/komponente/admin/Dashboard.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 
 const Dashboard = () => {
-  return <div style={{textAlign: 'center'}}>
-    <h2 style={{fontStyle: 'italic'}}>Hello Anđela! Welcome to Dashboard!</h2>
-    <img src="https://i.pinimg.com/564x/a6/58/32/a65832155622ac173337874f02b218fb.jpg" class="img-thumbnail" alt="img-thumbnail"
-    style={{width: '200px', height: '200px', margin: '10px', borderColor: 'white'}} />
-
-
-  </div>
+  return (
+    <div
+      style={{ textAlign: "center" }}
+    >
+      <h2
+        style={{ fontStyle: "italic" }}
+      >
+        Hello Admin! Welcome to
+        Dashboard!
+      </h2>
+      <img
+        src="https://i.pinimg.com/564x/a6/58/32/a65832155622ac173337874f02b218fb.jpg"
+        class="img-thumbnail"
+        alt="img-thumbnail"
+        style={{
+          width: "200px",
+          height: "200px",
+          margin: "10px",
+          borderColor: "white",
+        }}
+      />
+    </div>
+  );
 };
 
 export default Dashboard;
